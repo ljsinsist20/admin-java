75ec963599388d78823659313fff3af5c061249d
ljsinsist20 <2593161116@qq.com>
2021-11-30 19:10:20 +0800

    final18-整合redis

--- src/main/java/com/ljs/game/controller/LoginController.java
+++ src/main/java/com/ljs/game/controller/LoginController.java
@@ -4,10 +4,12 @@ import com.ljs.game.pojo.entity.Admin;
 import com.ljs.game.result.R;
 import com.ljs.game.service.LoginService;
 import com.ljs.game.utils.JwtUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/admin")
@@ -17,6 +19,9 @@ public class LoginController {
     @Resource
     private LoginService loginService;
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @PostMapping("/login")
     public R login(@RequestBody Admin admin) {
         Admin adminFlag = loginService.login(admin.getUserName(), admin.getPassWord());
@@ -24,7 +29,18 @@ public class LoginController {
             String token = JwtUtils.createToken(adminFlag.getId(), adminFlag.getUserName());
             return R.ok().data("token", token);
         }
-        return R.error().message("用户不存在");
+        Integer a = (Integer) redisTemplate.opsForValue().get(admin.getUserName());
+        if (a == null) {
+            a = 0;
+            redisTemplate.opsForValue().set(admin.getUserName(), a, 30, TimeUnit.MINUTES);
+        }
+        if (a < 5) {
+            a++;
+            redisTemplate.opsForValue().set(admin.getUserName(), a, 30, TimeUnit.MINUTES);
+        } else {
+            return R.error().message("当前用户锁定");
+        }
+        return R.error().message("用户名或密码错误");
     }
 
     @GetMapping("/logout")
